d44df91c69ee3ee14eaebe3191652a3bb5a3c9c6
Ben Aldrich <benjaminaldrich9@gmail.com>
2023-08-26 02:46:22 +0100

    Update ItemHandler.cpp
    Remove the fix we suggested for the client side reforge item level fix cheat

--- src/server/game/Handlers/ItemHandler.cpp
+++ src/server/game/Handlers/ItemHandler.cpp
@@ -1499,12 +1499,12 @@ void WorldSession::HandleReforgeItemOpcode(WorldPacket& recvData)
     }
 
     // Packet spoofing check. The client does not allow reforging reforging items below item level 200
-    if (item->GetItemLevel() < 200)
-    {
-        TC_LOG_ERROR("network", "WORLD: HandleReforgeItemOpcode - Player (Guid: %u Name: %s) tried to reforge an item item (Entry: %u) below item level 200. Probably cheater.", player->GetGUID().GetCounter(), player->GetName().c_str(), item->GetEntry());
-        SendReforgeResult(false);
-        return;
-    }
+    //if (item->GetItemLevel() < 200)
+    //{
+       // TC_LOG_ERROR("network", "WORLD: HandleReforgeItemOpcode - Player (Guid: %u Name: %s) tried to reforge an item item (Entry: %u) below item level 200. Probably cheater.", player->GetGUID().GetCounter(), player->GetName().c_str(), item->GetEntry());
+        //SendReforgeResult(false);
+        //return;
+    //}
 
     if (!reforgeEntry)
     {
